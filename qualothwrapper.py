554a1b26d4ec6275692a659db82d29d47e9c786b
maria-korosteleva <maria@korosteleva.com>
2020-04-08 17:41:54 +0900

    autodetect maya version year

--- qualothwrapper.py
+++ qualothwrapper.py
@@ -3,20 +3,22 @@
     This module makes a python interface to them
     Note that's Python 2.7-friendly
 """
+from __future__ import print_function
 
 from maya import mel
 from maya import cmds
 
-# Note: the name is Maya-vesion dependent
-# TODO auto detect maya version
-plugin_name = 'qualoth_2020_x64'
-
 
 def load_plugin():
     """
-        Forces loading Qualoth plugin into Maya. Note that you need a license to use it!
+        Forces loading Qualoth plugin into Maya. 
+        Note that plugin should be installed and licensed to use it!
         Inquire here: http://www.fxgear.net/vfxpricing
     """
+    maya_year = mel.eval('getApplicationVersionAsFloat')
+    plugin_name = 'qualoth_' + str(maya_year) + '_x64'
+    print('Loading ', plugin_name)
+    
     cmds.loadPlugin(plugin_name)
 
 

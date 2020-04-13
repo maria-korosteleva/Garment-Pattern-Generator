5fcf7bd1f773ddbd8930d67433e827369d3d3c90
maria-korosteleva <maria@korosteleva.com>
2020-04-13 17:38:35 +0900

    setup static equilibrium checks

--- mayamain.py
+++ mayamain.py
@@ -11,6 +11,7 @@ import time
 
 # Maya
 from maya import cmds
+from maya import OpenMaya
 
 # My module
 import pattern.core as core
@@ -19,7 +20,7 @@ reload(core)
 reload(qw)
 
 
-class MayaPattern(core.BasicPattern):
+class MayaGarment(core.BasicPattern):
     """
     Extends a pattern specification in custom JSON format to work with Maya
         Input:
@@ -29,14 +30,19 @@ class MayaPattern(core.BasicPattern):
         * Basic operations on panels in Maya TODO
     """
     def __init__(self, pattern_file):
-        super(MayaPattern, self).__init__(pattern_file)
+        super(MayaGarment, self).__init__(pattern_file)
         self.loaded_to_maya = False
+        self.maya_shape = None
+        self.maya_cloth_object = None
+        self.last_verts = None
+        self.current_verts = None
     
     def load(self, parent_group='experiment'):
         """
             Loads current pattern to Maya as curve collection.
             Groups them by panel and by pattern
         """
+        # Load panels as curves
         maya_panel_names = []
         for panel_name in self.pattern['panels']:
             panel_maya = self._load_panel(panel_name)
@@ -48,7 +54,74 @@ class MayaPattern(core.BasicPattern):
         self.pattern['maya'] = group_name  # Maya modifies specified name for uniquness
         
         print("All panels loaded to Maya")
+
+        # stitch them
+        self._stitch_panels()
+
         self.loaded_to_maya = True
+
+    def get_qlcloth_geomentry(self):
+        """
+            Find the first Qualoth cloth geometry object belonging to current pattern
+        """
+        if not self.maya_shape:
+            children = cmds.listRelatives(self.pattern['maya'], ad=True)
+            cloths = [obj for obj in children 
+                      if 'qlCloth' in obj and 'Out' in obj and 'Shape' in obj]
+            self.maya_shape = cloths[0]
+
+        return self.maya_shape
+
+    def get_qlcloth_props_obj(self):
+        """
+            Find the first qlCloth object belonging to current pattern
+        """
+        if not self.maya_cloth_object:
+            children = cmds.listRelatives(self.pattern['maya'], ad=True)
+            cloths = [obj for obj in children 
+                      if 'qlCloth' in obj and 'Out' not in obj and 'Shape' in obj]
+            self.maya_cloth_object = cloths[0]
+
+        return self.maya_cloth_object
+
+    def update_verts_info(self):
+        """
+            Retrieves current vertex positions from Maya & updates the last state.
+            For best performance, should be called on each iteration of simulation
+            Assumes the object is already loaded & stitched
+        """
+        # working with meshes http://www.fevrierdorian.com/blog/post/2011/09/27/Quickly-retrieve-vertex-positions-of-a-Maya-mesh-%28English-Translation%29
+        cloth_dag = name_to_dag(self.get_qlcloth_geomentry())
+        
+        mesh = OpenMaya.MFnMesh(cloth_dag)
+        maya_vertices = OpenMaya.MPointArray()
+        mesh.getPoints(maya_vertices, OpenMaya.MSpace.kWorld)
+
+        vertices = np.empty((maya_vertices.length(), 3))
+        for i in range(maya_vertices.length()):
+            for j in range(3):
+                vertices[i, j] = maya_vertices[i][j]
+
+        self.last_verts = self.current_verts
+        self.current_verts = vertices
+
+    def is_static(self, threshold):
+        """
+            Checks wether garment is in the static equilibrium
+            Compares current state with the last recorded state
+        """
+        if self.last_verts is None:  # first iteration
+            return False
+        
+        # Compare L1 norm per vertex
+        # Checking vertices change is the same as checking if velocity is zero
+        diff = np.abs(self.current_verts - self.last_verts)
+        diff_L1 = np.sum(diff, axis=1)
+        # DEBUG print(np.sum(diff), threshold * len(diff))
+        if (diff_L1 < threshold).all():  # compare vertex-vize to be independent of #verts
+            return True
+        else:
+            return False
 
     def _load_panel(self, panel_name):
         """
@@ -110,16 +183,14 @@ class MayaPattern(core.BasicPattern):
 
         return list(map(tuple, points))
 
-    def stitch_panels(self):
+    def _stitch_panels(self):
         """
             Create seams between qualoth panels.
-            Calls to load panels if they are not already loaded.
-            Assumes that after stitching the pattern becomes a single piece of geometry
+            Assumes that panels are already loadeded (as curves).
+            Assumes that after stitching every pattern becomes a single piece of geometry
             Returns
                 Qulaoth cloth object name
         """
-        if not self.loaded_to_maya:
-            self.load('stitching')
 
         for stitch in self.pattern['stitches']:
             from_curve = self._maya_curve_name(stitch['from'])
@@ -150,26 +221,6 @@ class MayaPattern(core.BasicPattern):
         edge_id = address['edge']
         return self.pattern['panels'][panel_name]['edges'][edge_id]['maya']
 
-    def get_qlcloth_geomentry(self):
-        """
-            Find the first Qualoth cloth geometry object belonging to current pattern
-        """
-        children = cmds.listRelatives(self.pattern['maya'], ad=True)
-        cloths = [obj for obj in children 
-                  if 'qlCloth' in obj and 'Out' in obj and 'Shape' in obj]
-
-        return cloths[0]
-
-    def get_qlcloth_props_obj(self):
-        """
-            Find the first qlCloth object belonging to current pattern
-        """
-        children = cmds.listRelatives(self.pattern['maya'], ad=True)
-        cloths = [obj for obj in children 
-                  if 'qlCloth' in obj and 'Out' not in obj and 'Shape' in obj]
-
-        return cloths[0]
-
 
 def load_body_as_collider(filename, garment, experiment):
     solver = qw.findSolver()
@@ -188,8 +239,15 @@ def load_body_as_collider(filename, garment, experiment):
     return body[0]
 
 
-def is_static(garment):
-    return False
+def name_to_dag(name):
+    """returns a dag path given a name
+        https://help.autodesk.com/view/MAYAUL/2016/ENU/?guid=__files_Maya_Python_API_Using_the_Maya_Python_API_htm
+    """
+    selectionList = OpenMaya.MSelectionList()
+    selectionList.add(name)
+    dagPath = OpenMaya.MDagPath()
+    selectionList.getDagPath(0, dagPath)
+    return dagPath
 
 
 def save_mesh(garment, save_to):
@@ -210,6 +268,17 @@ def record_fail(current_path, props):
     props['sim_fails'].append(name)
 
 
+def init_sim(solver, props):
+    """
+        Basic simulation settings before starting simulation
+    """
+    cmds.setAttr(solver + '.selfCollision', 1)
+    cmds.setAttr(solver + '.startTime', 1)
+    cmds.setAttr(solver + '.solverStatistics', 0)  # for easy reading of console output
+    cmds.playbackOptions(ps=0, max=props['max_sim_steps'])  # 0 playback speed = play every frame
+    cmds.currentTime(1)  # needed to suppress "Frames skipped warning"
+
+
 def run_sim(garment, body, experiment, save_to, props):
     """
         Setup and run simulation of the garment on the body
@@ -217,27 +286,26 @@ def run_sim(garment, body, experiment, save_to, props):
     """
     solver = qw.findSolver()
 
-    # properties
-    cmds.setAttr(solver + '.selfCollision', 1)
-    cmds.setAttr(solver + '.startTime', 1)
-    cmds.setAttr(solver + '.solverStatistics', 0)  # for easy reading of console output
-    cmds.playbackOptions(ps=0, max=props['max_sim_steps'])  # 0 playback speed = play every frame
-    cmds.currentTime(1)  # needed to suppress "Frames skipped warning"
+    # init
+    init_sim(solver, props)
 
     # run -- Manual "play"
     # NOTE! It will run simulate all cloth existing in the scene
     start_time = time.time()
     for frame in range(1, props['max_sim_steps']):
         cmds.currentTime(frame)  # step
-        if is_static(garment):  # TODO Add penetration checks
-            save_mesh(save_to)
+        garment.update_verts_info()
+        if garment.is_static(props['static_threshold']):  # TODO Add penetration checks
+            save_mesh(garment, save_to)
             render(save_to)
             break
     # Record time to sim + fps (or Sec per Frame =))
     # TODO make recording pattern-specific, not dataset-specific
-    props['spf'] = (time.time() - start_time) / frame
+    props['sim_time'] = (time.time() - start_time)
+    props['spf'] = props['sim_time'] / frame
+    props['fin_frame'] = frame
 
-    # static equilibrium never detected
+    # static equilibrium never detected -- might have false negs!
     if frame == props['max_sim_steps'] - 1:
         record_fail(save_to, props)
     
@@ -266,8 +334,9 @@ def main():
 
     try:
         sim_options = {
-            'max_sim_steps': 20, 
-            'sim_fails': []
+            'max_sim_steps': 1500, 
+            'sim_fails': [], 
+            'static_threshold': 0.01  # depends on the units used
         }
 
         qw.load_plugin()
@@ -275,22 +344,21 @@ def main():
         # --- future loop of batch processing ---
         experiment_name = start_experiment('test')
 
-        pattern = MayaPattern(
+        garment = MayaGarment(
             'C:/Users/LENOVO/Desktop/Garment-Pattern-Estimation/data_generation/Patterns/skirt_maya_coords.json'
         )
-        pattern.load(experiment_name)
-        pattern.stitch_panels()
-        pattern.setMaterialProps()
-        cloth_ref = pattern.get_qlcloth_geomentry()
+        garment.load(experiment_name)
+        garment.setMaterialProps()
 
         body_ref = load_body_as_collider(
             'F:/f_smpl_templatex300.obj', 
-            cloth_ref,
+            garment.get_qlcloth_geomentry(),
             experiment_name
         )
 
-        run_sim(cloth_ref, body_ref, 
-                experiment_name, 'F:/GK-Pattern-Data-Gen/Sims', 
+        run_sim(garment, body_ref, 
+                experiment_name, 
+                'F:/GK-Pattern-Data-Gen/Sims', 
                 sim_options)
 
         # Fin

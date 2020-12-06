bec28b446c5e117d42c9bcaa90ab9919c25a85e3
Maria Korosteleva <mariako@kaist.ac.kr>
2020-12-06 15:42:59 +0900

    No LR scheduling test

--- datascan.py
+++ datascan.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     path = system_config['templates_path']
 
     # ------ Dataset ------
-    dataset = 'data_1000_skirt_4_panels_200616-14-14-40'
+    dataset = 'data_5000_tee_200924-16-57-59'
     datapath = os.path.join(system_config['datasets_path'], dataset)
     dataset_file = os.path.join(datapath, 'dataset_properties.json')
     data_props = customconfig.Properties(dataset_file)

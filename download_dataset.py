5939363c69e7c0515044add49b83757a36c248ef
mariako <mariako@kaist.ac.kr>
2021-02-18 23:00:33 +0900

    experiment setup

--- download_dataset.py
+++ download_dataset.py
@@ -5,5 +5,5 @@ import customconfig
 system_props = customconfig.Properties('./system.json')
 
 api = wandb.Api({'project': 'Garments-Reconstruction'})
-artifact = api.artifact(name='dataset_1000_pants_straight_sides_210105-10-49-02:latest')
-filepath = artifact.download(Path(system_props['datasets_path']) / 'data_1000_pants_straight_sides_210105-10-49-02')
+artifact = api.artifact(name='dataset_1000_tee_200527-14-50-42_regen_200612-16-56-43:latest')
+filepath = artifact.download(Path(system_props['datasets_path']) / 'data_uni_1000_tee_200527-14-50-42_regen_200612-16-56-43')

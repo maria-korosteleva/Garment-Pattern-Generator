a9544326d897d2806bdc7a782955bfc85433d3ae
Maria Korosteleva <mariako@kaist.ac.kr>
2021-03-23 14:20:57 +0900

    hoody dataset prep

--- datagenerator.py
+++ datagenerator.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     if new:
         props = Properties()
         props.set_basic(
-            templates='combos/wb_dress_sleeveless.json',  # 'skirts/skirt_4_panels.json',  # 'pants/pants_straight_sides.json',  # 'basic tee/tee.json',
+            templates='basic tee/jacket_hood.json',  # 'skirts/skirt_4_panels.json',  # 'pants/pants_straight_sides.json',  # 'basic tee/tee.json',
             name='data_uni_300',
             size=300,
             to_subfolders=True)

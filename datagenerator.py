28505a8680795a16563b30e3961238589f8f7815
Maria Korosteleva <mariako@kaist.ac.kr>
2021-04-01 15:27:29 +0900

    closed hoody dataset

--- datagenerator.py
+++ datagenerator.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     if new:
         props = Properties()
         props.set_basic(
-            templates='skirts/skirt_waistband.json',  # 'skirts/skirt_4_panels.json',  # 'pants/pants_straight_sides.json',  # 'basic tee/tee.json',
+            templates='basic tee/tee_hood.json',  # 'skirts/skirt_4_panels.json',  # 'pants/pants_straight_sides.json',  # 'c.json',
             name='test_150',
             size=150,
             to_subfolders=True)

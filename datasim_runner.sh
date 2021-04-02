8ca9eb84c3017826914c1c9a9eaae7e27d848788
Maria Korosteleva <mariako@kaist.ac.kr>
2021-04-02 17:52:59 +0900

    waistband jumpsuit

--- datasim_runner.sh
+++ datasim_runner.sh
@@ -22,8 +22,8 @@ trap 'sigint'  INT
 # -- Main calls --
 num_samples=30   # number of reloads and re-sim vs. speed to detect Maya\Qualoth hang
 per_sample_delay=$((7*60))  # give about 7 min per sample before detecting Maya to hang
-dataset=test_150_skirt_waistband_210331-16-05-37
-config=wb_dress_custom_fabric_basic_body.json
+dataset=test_150_jumpsuit_210401-16-28-21
+config=jumpsuit_custom_fabric_basic_body.json
 ret_code=1
 STARTTIME=$(date +%s)
 while [ $ret_code != 0 ]  # failed for any reason

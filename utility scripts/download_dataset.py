239aee1e320c650bc739a517bcd759cbf54f6486
mariako <mariako@kaist.ac.kr>
2021-04-20 16:00:00 +0900

    double the batch

--- utility scripts/download_dataset.py
+++ utility scripts/download_dataset.py
@@ -5,5 +5,5 @@ import customconfig
 system_props = customconfig.Properties('./system.json')
 
 api = wandb.Api({'project': 'Garments-Reconstruction'})
-artifact = api.artifact(name='dataset_1000_pants_straight_sides_210105-10-49-02:latest')
-filepath = artifact.download(Path(system_props['datasets_path']) / 'data_uni_1000_pants_straight_sides_210105-10-49-02')
+artifact = api.artifact(name='jacket:latest')
+filepath = artifact.download(Path(system_props['datasets_path']) / 'data_1250_jacket_210414-15-48-45')

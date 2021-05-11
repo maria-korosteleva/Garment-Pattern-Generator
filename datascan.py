793a9bb437e3f0d6b327b2104fbe90a9af79feed
mariako <mariako@kaist.ac.kr>
2021-05-11 12:15:27 +0900

    datascan setup

--- datascan.py
+++ datascan.py
@@ -1,7 +1,8 @@
 """
     Emulate the result of scanning a garment on existing dataset of simulated garments
     
-    IMPORTANT!! This script need to be run with mayapy to get access to Autodesk Maya Python API. E.g., on Windows the command could look like this: 
+    IMPORTANT!! This script need to be run with mayapy to get access to Autodesk Maya Python API. 
+        E.g., on Windows the command could look like this: 
         d:/Autodesk/Maya2020/bin/mayapy.exe "./data_generation/datascan.py"
     """
 
@@ -45,13 +46,16 @@ if __name__ == "__main__":
     system_config = customconfig.Properties('system.json')  # Make sure it's in \Autodesk\MayaNNNN\
     path = system_config['templates_path']
 
-    # ------ Dataset ------
-    dataset = 'data_25_tee_210122-15-53-54'
-    datapath = os.path.join(system_config['datasets_path'], dataset)
-    dataset_file = os.path.join(datapath, 'dataset_properties.json')
-    data_props = customconfig.Properties(dataset_file)
-    if not data_props['to_subfolders']:
-        raise NotImplementedError('Scanning only works on datasets organized in subfolders')  # just for simplicity 
+    # ------ Datasets ------
+    dataset_folders = [
+        'test_150_dress_210401-17-57-12',
+        'test_150_jacket_hood_sleeveless_210331-11-16-33',
+        'test_150_jacket_sleeveless_210331-15-54-26',
+        'test_150_jumpsuit_210401-16-28-21',
+        'test_150_skirt_waistband_210331-16-05-37',
+        'test_150_tee_hood_210401-15-25-29',
+        'test_150_wb_jumpsuit_sleeveless_210404-11-27-30'
+    ]
 
     # ------ Start Maya instance ------
     init_mayapy()
@@ -59,52 +63,60 @@ if __name__ == "__main__":
     reload(mymaya)  # reload in case we are in Maya internal python environment
     from mayaqltools import utils
 
-    # ------ Main loop --------
-    if not 'scan_imitation' in data_props:
-        number_of_rays = 30
-        number_of_visible_rays = 4
-        data_props.set_section_config('scan_imitation', test_rays_num=number_of_rays, visible_rays_num=number_of_visible_rays)
-
     # load body to the scene
     body = utils.load_file(os.path.join(system_config['bodies_path'], data_props['body']), 'body')
     utils.scale_to_cm(body)
-    
-    # go over the examples in the data
-    start_time = time.time()
-    to_ignore = ['renders']  # special dirs not to include in the pattern list
-    root, dirs, files = next(os.walk(datapath))  # cannot use os.scandir in python 2.7
 
-    data_props.set_section_stats('scan_imitation', faces_removed={}, processing_time={})
-    for name in dirs:
-        if name not in to_ignore:
-            dir_path = os.path.join(root, name)
-            # load mesh
-            garment = utils.load_file(os.path.join(dir_path, name + '_sim.obj'), name + '_sim')
-            
-            # do what we are here for
-            removed, time_taken = mymaya.scan_imitation.remove_invisible(
-                garment, [body],
-                data_props['scan_imitation']['config']['test_rays_num'], 
-                data_props['scan_imitation']['config']['visible_rays_num'])
-            data_props['scan_imitation']['stats']['faces_removed'][name] = removed
-            data_props['scan_imitation']['stats']['processing_time'][name] = time_taken
+    for dataset in dataset_folders:
+        datapath = os.path.join(system_config['datasets_path'], dataset)
+        dataset_file = os.path.join(datapath, 'dataset_properties.json')
+        data_props = customconfig.Properties(dataset_file)
+        if not data_props['to_subfolders']:
+            raise NotImplementedError('Scanning only works on datasets organized in subfolders')  # just for simplicity 
 
-            # save to original folder
-            utils.save_mesh(garment, os.path.join(dir_path, name + '_scan_imitation.obj'))
-            
-            cmds.delete(garment)  # cleanup
+        # ------ Main loop --------
+        if 'scan_imitation' not in data_props:
+            number_of_rays = 30
+            number_of_visible_rays = 4
+            data_props.set_section_config(
+                'scan_imitation', test_rays_num=number_of_rays, visible_rays_num=number_of_visible_rays)
+        
+        # go over the examples in the data
+        start_time = time.time()
+        to_ignore = ['renders']  # special dirs not to include in the pattern list
+        root, dirs, files = next(os.walk(datapath))  # cannot use os.scandir in python 2.7
 
-    # update props & save
-    passed = time.time() - start_time
-    data_props.summarize_stats('processing_time', log_sum=True, log_avg=True, as_time=True)
-    data_props.summarize_stats('faces_removed', log_avg=True)
-    data_props.set_section_stats(
-        'scan_imitation', total_processing_time=str(timedelta(seconds=passed))
-    )
-    
-    data_props.serialize(dataset_file)
+        data_props.set_section_stats('scan_imitation', faces_removed={}, processing_time={})
+        for name in dirs:
+            if name not in to_ignore:
+                dir_path = os.path.join(root, name)
+                # load mesh
+                garment = utils.load_file(os.path.join(dir_path, name + '_sim.obj'), name + '_sim')
+                
+                # do what we are here for
+                removed, time_taken = mymaya.scan_imitation.remove_invisible(
+                    garment, [body],
+                    data_props['scan_imitation']['config']['test_rays_num'], 
+                    data_props['scan_imitation']['config']['visible_rays_num'])
+                data_props['scan_imitation']['stats']['faces_removed'][name] = removed
+                data_props['scan_imitation']['stats']['processing_time'][name] = time_taken
 
-    print('Scan imitation on {} performed successfully!!!'.format(dataset))
+                # save to original folder
+                utils.save_mesh(garment, os.path.join(dir_path, name + '_scan_imitation.obj'))
+                
+                cmds.delete(garment)  # cleanup
+
+        # update props & save
+        passed = time.time() - start_time
+        data_props.summarize_stats('processing_time', log_sum=True, log_avg=True, as_time=True)
+        data_props.summarize_stats('faces_removed', log_avg=True)
+        data_props.set_section_stats(
+            'scan_imitation', total_processing_time=str(timedelta(seconds=passed))
+        )
+        
+        data_props.serialize(dataset_file)
+
+        print('Scan imitation on {} performed successfully!!!'.format(dataset))
 
     # End Maya instance
     stop_mayapy()  # ensures correct exit without errors

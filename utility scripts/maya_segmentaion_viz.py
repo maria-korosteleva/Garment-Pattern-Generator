f872b979b1fd9b3f842a8e3b938389707912f205
mariako <mariako@kaist.ac.kr>
2021-05-17 17:04:15 +0900

    separate script and faster mesh colors

--- utility scripts/maya_segmentaion_viz.py
+++ utility scripts/maya_segmentaion_viz.py
@@ -8,6 +8,8 @@ import os
 import numpy as np
 import time
 
+from numpy.lib.arraysetops import unique
+
 # Maya
 import maya.cmds as cmds
 
@@ -24,33 +26,39 @@ num_verts = cmds.polyEvaluate(garment, vertex=True)
 
 # load labels
 with open(segmentation_path, 'r') as f:
-    vert_labels = [line for line in f]
-
+    vert_labels = [line.rstrip() for line in f]  # remove \n
 unique_labels = list(set(vert_labels))  # all unique labels available
 
-# Coloring vertices for visualization
+start_time = time.time()
+
+# group vertices by label
+vertex_select_lists = dict.fromkeys(unique_labels)
+for key in vertex_select_lists:
+    vertex_select_lists[key] = []
+for vert_idx in range(num_verts):
+    str_label = vert_labels[vert_idx]
+    vert_addr = '{}.vtx[{}]'.format(garment, vert_idx)
+    vertex_select_lists[str_label].append(vert_addr)
+
+# Base colors
 # https://www.schemecolor.com/bright-rainbow-colors.php
 color_hex = ['FF0900', 'FF7F00', 'FFEF00', '00F11D', '0079FF', 'A800FF']
 color_list = np.empty((len(color_hex), 3))
 for idx in range(len(color_hex)):
     color_list[idx] = np.array([int(color_hex[idx][i:i + 2], 16) for i in (0, 2, 4)]) / 255.0
 
-start_time = time.time()
-for i in range(num_verts):
-    # Color according to evaluated label!
-    str_label = vert_labels[i]
+# color them
+for label, str_label in enumerate(unique_labels):
+    # color selection with expansion if the list is too small
+    factor, color_id = (label // len(color_list)) + 1, label % len(color_list)
+    color = color_list[color_id] / factor  # gets darker the more labels there are
 
-    if str_label is None or 'None' in str_label:  # non-segmented becomes black
-        color = np.zeros(3)
-    else:
-        label = unique_labels.index(str_label)
+    # color corresponding vertices
+    cmds.select(clear=True)
+    cmds.select(vertex_select_lists[str_label])
+    cmds.polyColorPerVertex(rgb=color.tolist())
 
-        # color selection with expnasion if the list is too small
-        factor, color_id = (label // len(color_list)) + 1, label % len(color_list)
-        color = color_list[color_id] / factor  # gets darker the more labels there are
-
-    cmds.polyColorPerVertex(garment + '.vtx[%d]' % i, rgb=color.tolist())
-
+cmds.select(clear=True)
 cmds.setAttr(garment + '.displayColors', 1)
 cmds.refresh()
 print('Colorization: ', time.time() - start_time)

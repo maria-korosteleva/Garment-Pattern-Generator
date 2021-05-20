033cdb813bb63a4e5670f7aa79bf14d210876a6f
mariako <mariako@kaist.ac.kr>
2021-05-20 17:36:31 +0900

    download latest additions to data

--- utility scripts/download_dataset.py
+++ utility scripts/download_dataset.py
@@ -13,12 +13,20 @@ to_download = {
     # 'jumpsuit-test:v0': 'test_150_jumpsuit_210401-16-28-21',
     # 'wb_jumpsuit_sleeveless-test:v0': 'test_150_wb_jumpsuit_sleeveless_210404-11-27-30',
     # 'tee_hood-test:v0': 'test_150_tee_hood_210401-15-25-29'
-    'multi-data:v66': 'Tee-JS-segment-shuffle-orderless-125',
-    'multi-data:v67': 'Tee-JS-segment-shuffle-orderless-25',
+    'jacket_hood:latest': 'data_1000_jacket_hood_210430-15-12-19',
+    # 'jacket_hood:v4': 'merged_jacket_hood_1700_210425-21-23-19',
+    'tee_sleeveless:latest': 'data_650_tee_sleeveless_210429-10-55-00',  # v2 I already have!
+    'jacket:latest': 'data_650_jacket_210504-11-07-51',
+    # 'jacket:v2': 'merged_jacket_1550_210420-16-54-04',
+    'wb_dress_sleeveless:latest': 'data_1050_wb_dress_sleeveless_210420-13-00-16',
+    'skirt_2_panels:latest': 'data_500_skirt_2_panels_210503-13-37-39',
+    'wb_pants_straight:latest': 'data_350_wb_pants_straight_210506-18-50-3',
+    # 'skirt_8_panels:v1': 'merged_skirt_8_panels_950_210412-16-11-33',
+    'skirt_8_panels:latest': 'data_50_skirt_8_panels_210511-12-56-59'
 }
 
 api = wandb.Api({'project': 'Garments-Reconstruction'})
 for key in to_download:
     artifact = api.artifact(name=key)
-    # filepath = artifact.download(Path(system_props['datasets_path']) / to_download[key])
-    filepath = artifact.download(Path(system_props['output']) / to_download[key])
+    filepath = artifact.download(Path(system_props['datasets_path']) / to_download[key])
+    # filepath = artifact.download(Path(system_props['output']) / to_download[key])

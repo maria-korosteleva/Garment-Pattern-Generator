0dc3022e76e130dff5ae94308357d287a98377bb
Maria Korosteleva <mariako@kaist.ac.kr>
2021-05-21 15:35:44 +0900

    fixes & check for unexpected files

--- utility scripts/all_data_has_all_files.py
+++ utility scripts/all_data_has_all_files.py
@@ -26,6 +26,7 @@ file_keys = [
     # 'scan_imitation.obj' sim_segmentation.txt, imitation_segmentation.txt..
 ]
 ignore_fails = True
+check_extra = False
 
 # ------ Datasets ------
 dataset_folders = []
@@ -40,9 +41,13 @@ for child in path.iterdir():
 
 print(dataset_folders)
 
+# gather errors here
 missing_files = dict.fromkeys(dataset_folders)
 for key in missing_files:
     missing_files[key] = []
+extra_files = dict.fromkeys(dataset_folders)
+for key in extra_files:
+    extra_files[key] = []
 size_errors = []
 render_errors = []
 
@@ -66,12 +71,19 @@ for dataset in dataset_folders:
 
             _, elem_dirs, elem_files = next(os.walk(dir_path))
 
+            # check all keys files are present
             for key in file_keys:
                 if not any(key in elem for elem in elem_files):
                     if not data_props.is_fail(name):
                         missing_files[dataset].append(name + '_' + key)
                     elif not ignore_fails:
                         missing_files[dataset].append(name + '_' + key + '_fail')
+            
+            # check if there are some other files present 
+            if check_extra:
+                for elem in elem_files:
+                    if not any(key in elem for key in file_keys):
+                        extra_files[dataset].append(name)
         
     # ------- Overall size checks -------
     data_size = data_props['size']
@@ -81,7 +93,7 @@ for dataset in dataset_folders:
         # missing elements check
         _, fails = data_props.count_fails()
         rendered = list(data_props['render']['stats']['render_time'].keys())
-        expected_datapoints = rendered + fails
+        expected_datapoints = set(rendered + fails)
         for name in dirs:
             if name not in to_ignore:
                 expected_datapoints.remove(name)
@@ -99,7 +111,7 @@ for dataset in dataset_folders:
         num_fails, _ = data_props.count_fails()
         expected_min_num = data_size * 2 - num_fails
         if num_renders != data_size * 2:
-            if num_renders > expected_min_num: 
+            if num_renders >= expected_min_num: 
                 render_errors.append('{}::Info:: Num renders {} is less then data size {}, but more then min expectation {}'.format(
                     dataset, num_renders, data_size * 2, expected_min_num))
             else:
@@ -129,6 +141,7 @@ for error in render_errors:
 print('\nFiles missing:')
 for dataset in dataset_folders:
     if len(missing_files[dataset]) > 0:
+        missing_files[dataset] = sorted(missing_files[dataset])
         print(dataset, ': ')
         for file in missing_files[dataset]:
             print(file)
@@ -137,10 +150,22 @@ for dataset in dataset_folders:
     else:
         no_file_problems.append(dataset)
 
+if check_extra:
+    print('\nExtra files:')
+    for dataset in dataset_folders:
+        if len(extra_files[dataset]) > 0:
+            extra_files[dataset] = sorted(extra_files[dataset])
+            print(dataset, ': ')
+            for file in extra_files[dataset]:
+                print(file)
+
+            print('->')
+
+
 # ----- Success ------
 no_problems = [d for d in dataset_folders if d in no_size_probelms and d in no_render_problems and d in no_file_problems]
 print('\nEverything OK: ')
-print(no_problems)
+print(*no_problems, sep='\n')
 
 # print('\nData size OK: ')
 # print(no_size_probelms)

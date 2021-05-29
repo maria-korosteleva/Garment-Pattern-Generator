90255d1b9a7930345c3103872945ce4c646d0ba1
mariako <mariako@kaist.ac.kr>
2021-05-29 19:32:01 +0900

    re-arrange things for separation

---
+++ packages/mayaqltools/__init__.py
@@ -0,0 +1,27 @@
+"""
+    Package for to simulate garments from patterns in Maya with Qualoth
+    Note that Maya uses Python 2.7 (incl Maya 2020) hence this package is adapted to Python 2.7
+
+    Main dependencies:
+        * Maya 2018+
+        * Arnold Renderer
+        * Qualoth (compatible with your Maya version)
+    
+    To run the package in Maya don't foget to add it to PYTHONPATH!
+"""
+import mayascene
+reload(mayascene)
+
+from .mayascene import MayaGarment, Scene, MayaGarmentWithUI
+
+import simulation
+import qualothwrapper
+import garmentUI
+import scan_imitation
+import utils
+
+reload(simulation)
+reload(qualothwrapper)
+reload(garmentUI)
+reload(scan_imitation)
+reload(utils)

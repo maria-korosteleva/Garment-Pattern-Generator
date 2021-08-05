57fc7b00b1753cf6723c06cf22bf8d8068017423
mariako <mariako@kaist.ac.kr>
2021-08-05 21:48:12 +0900

    load maya plugins for Garment Viewer

--- data_generation/garmentviewer.py
+++ data_generation/garmentviewer.py
@@ -5,13 +5,19 @@
     * Qualoth
 """
 
+from maya import cmds
+
 # My modules
 import mayaqltools as mymaya
 reload(mymaya)
 
 # -------------- Main -------------
 if __name__ == "__main__":
+    print('Load plugins')
     mymaya.qualothwrapper.load_plugin()
+    cmds.loadPlugin('mtoa.mll')  # https://stackoverflow.com/questions/50422566/how-to-register-arnold-render
+    cmds.loadPlugin('objExport.mll')  # same as in https://forums.autodesk.com/t5/maya-programming/invalid-file-type-specified-atomimport/td-p/9121166
+
     try:
         mymaya.garmentUI.start_GUI()
     except Exception as e:
